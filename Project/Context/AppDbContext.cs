e52c58044e8033094bc3ae57e68d8196df6baf4a
anjali <anjaliyerra99@gmail.com>
2023-06-13 19:40:19 +0530

    first commit

---
+++ Project/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Models;
+namespace Project.Context
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        { 
+        }
+        public DbSet<User> Users { get; set; }
+         protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");
+
+        }
+
+       
+
+
+    }
+}
